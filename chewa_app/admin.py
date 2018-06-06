b129ed53d84c199c074f7a376c17e9746ab92959
mugala <mugalalevi21@gmail.com>
2018-06-07 00:39:45 +0300

    Register models at admin
    This commit will register the models at admin

--- chewa_app/admin.py
+++ chewa_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Language,Profile,Level,Lessons,Content
 
 # Register your models here.
+
+
+class LessonsAdmin(admin.ModelAdmin):
+    filter_horizontal = ('Language',)
+
+
+admin.site.register(Language)
+admin.site.register(Profile)   
+admin.site.register(Level)   
+admin.site.register(Lessons)   
+admin.site.register(Content)   
