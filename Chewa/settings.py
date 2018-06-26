4d550e7e2fdf52ad28d34dc3dad1b7961e4266e5
mugala <mugalalevi21@gmail.com>
2018-06-26 14:39:12 +0300

    new styles to make the page responsive as well s fixing the hambuger menu

--- Chewa/settings.py
+++ Chewa/settings.py
@@ -21,11 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'SECRET_KEY'
+# SECRET_KEY = config('SECRET_KEY')
 
 # # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = config('DEBUG', default=False, cast=bool)
-DEBUG = True
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = ['chewa.herokuapp.com']
 
 
@@ -123,8 +124,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'chewa',
-        'USER': 'kiptim',
-        'PASSWORD': 'jerotich',
+        'USER': 'levi',
+        'PASSWORD': 'eyeshadow',
     }
 }
 
